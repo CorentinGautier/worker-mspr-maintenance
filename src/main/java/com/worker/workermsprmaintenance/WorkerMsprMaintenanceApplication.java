a4656f401c27ab8f00ac99a8a6fc8fabcf69a422
Corentin <cortech12@gmail.com>
2021-04-12 13:06:27 +0200

    init project

---
+++ src/main/java/com/worker/workermsprmaintenance/WorkerMsprMaintenanceApplication.java
@@ -0,0 +1,13 @@
+package com.worker.workermsprmaintenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkerMsprMaintenanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkerMsprMaintenanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/worker/workermsprmaintenance/WorkerMsprMaintenanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worker.workermsprmaintenance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkerMsprMaintenanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
